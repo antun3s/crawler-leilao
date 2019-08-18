97392597e5fd589c0c622c87e47435128eb0daa2
Bruno Antunes <antunes.dll@gmail.com>
2019-08-18 13:52:20 -0300

    Update crawler-basico-leilao.py
    Refeito a forma de capturar dados da página do produto.

--- crawler-basico-leilao.py
+++ crawler-basico-leilao.py
@@ -1,57 +1,75 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from requests import get
 from bs4 import BeautifulSoup
 import re
 
-url = 'https://www.freitasleiloeiro.com.br/leiloes/pesquisar?query=&categoria=2&subCategoria=&subCategoriaLabel=&estado=&cidade=&faixaValor=3&faixaValorLabel=De%20R$%2025.000%20At%C3%A9%20R$%2050.000&condicao=&patio='
-response = get(url)
 
-html_soup = BeautifulSoup(response.text, 'html.parser')
+# get product-lot - Ok
+# get product-views - Ok
+# get product-bid - Ok 
+# get product-name - Ok
+# get product-actual-price - Ok
+# get bid-username - Ok
+# get bit-type - Ok
 
-lista_item = html_soup.find(id='table_agenda_body')
+product_url = 'https://www.freitasleiloeiro.com.br/leiloes/lote?leilaoid=3790&lote=36'
+response = get(product_url)
+product_soup = BeautifulSoup(response.text, 'html.parser')
 
-# find abaixo busca pelo class específica
-rows = lista_item.find_all(class_ ='cursor-pointer')
+def get_product_lot():
+    lot_soup = product_soup.find(id="num-lote")
+    product_lot = lot_soup.h4.text 
+    print(product_lot)
 
-i = 0
-for row in rows:
-    item = rows[i]
+def get_product_view():
+    view_soup = product_soup.find_all(class_="col-sm-1 text-center")
+    product_view = view_soup[0].h4.text
+    print(product_view)
 
-    # data_hora_consulta
-    data_hora_consulta = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    
-    # captura Lote
-    lote=item.h3.text
+def get_bid_number():
+    bid_soup = product_soup.find(id="lblTotalLances")
+    print(bid_soup.text)
 
-    # extrai os dados de texto longo e separado em linhas
-    # captura todo o texto da div que tem a "class text-justify"
-    nome_bruto=item.find("div",{"class":"text-justify;"}).text
-    # separa linha do data_hora do leilao
-    nome_bruto = re.sub(' - ', '\n',nome_bruto)
-    # remove espaços repetidos
-    nome_bruto= re.sub(' +', ' ',nome_bruto)
-    # separa o nome bruto, cada linha vira um item de lista
-    nome_bruto = nome_bruto.splitlines()
-    
-    # captura data_hora do leilao
-    data_hora_leilao = nome_bruto[1]
-    
-    # captura local do leilao
-    local = nome_bruto[2]
-    
-    # captura nome do item em leilao
-    nome = nome_bruto[5]
-    
-    # Lance
-    # Coleta lance com R$ e remove o . do milhar
-    lance = item.tr.td.text.replace('.','')
-    # prepara a string
-    lance = lance[3:].replace(',','.')
-     
-    #Imprime no log
-    with open('consulta.log', 'a') as consulta:
-        consulta.write('"{}","{}","{}","{}","{}","{}";\n'.format(data_hora_consulta,lote,data_hora_leilao,local,nome,lance))
-    
-    i = i + 1    
+def get_name():
+    product_name = ' '.join(product_soup.h2.text.split())
+    print(product_name)
+
+def get_bid_soup():
+    # busca por scripts do tipo "text/javascript"
+    price_soup = product_soup.find_all("script", type="text/javascript")
+    # dentro do script ele busca a linha com o link do lance, e converte de lista para string
+    bid_url = ''.join(map(str,(re.findall('"([^"]*)"',re.findall('.*lanceslote.*',price_soup[1].text)[0]))))
+    bid_url = "https://www.freitasleiloeiro.com.br/" + bid_url
+    # print(bid_url)
+    bid_response = get(bid_url)
+    bid_soup = BeautifulSoup(bid_response.text, 'html.parser')
+    return(bid_soup)
+
+def get_bid_price():
+    bid_soup = get_bid_soup()
+    # remove simbolo monetario e centavos, após isto converte para inteiro
+    product_bid_price = int(bid_soup.h2.text[3:-3].replace('.',''))
+    print(product_bid_price)
+
+def get_bid_username():
+    bid_soup = get_bid_soup()
+    product_bid_username = bid_soup.find_all("tr")[0].td.text
+    print(product_bid_username)
+
+def get_bid_type():
+    bid_soup = get_bid_soup()
+    product_bid_type = bid_soup.find_all("tr")[2].td.text
+    print(product_bid_type)
+
+def get_bid_date():
+    bid_soup = get_bid_soup()
+    product_bid_date = bid_soup.find_all("tr")[1].td.text
+    print(product_bid_date)
+
+get_product_lot()
+get_product_view()
+get_bid_number()
+get_name()
+get_bid_price()
+get_bid_username()
+get_bid_type()
+get_bid_date()
